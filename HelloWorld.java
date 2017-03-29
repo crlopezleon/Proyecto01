a5c9bf21b333ed240e6dcc5a27d074a40c79885c
crlopezleon <crlopez@softtek.com>
2017-03-29 17:34:42 -0600

    Commit en el branch Bug01

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String mensajeGit = "Hello GitHub!";
+		String mensajeGit = "Hello Git a bug was fixed!!";
 		
 		/* String mensajeGit = "Hello Git!";
 		 * String mensajeGit = "Hello GitHub!";
